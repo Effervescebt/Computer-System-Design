a191191a0bdbafbccc649e99bf58a0066caba2e4
Haoru Li <haorul2@illinois.edu>
2024-11-21 16:24:35 -0600

    draft version 3.0

--- src/kern/process.c
+++ src/kern/process.c
@@ -55,10 +55,6 @@ char procmgr_initialized = 0;
  * 
  */
 void procmgr_init(void){
-    // for (int i = 0; i < NPROC; i++) {
-    //     proctab[i] = NULL;
-    // }
-    // First clear the process struct
     memset(&main_proc, 0, sizeof(main_proc));
     // Initialize PID to 0
     main_proc.id = MAIN_PID;
@@ -76,7 +72,16 @@ void procmgr_init(void){
 }
 
 /**
- * @brief: 
+ * @brief: This function takes I/O interface and the execute the program it refer to.
+ * @param: exeio: the passed I/O interface
+ * 
+ * This function performs the following steps:
+ * 1. Unmap virtual memory mapping to other user process
+ * 2. Create a new memory space(not needed for this cp)
+ * 3. Load the executable from the passed-in I/O interface
+ * 4. Start the associated thread in U mode
+ * 
+ * @return: error code or 0 for success
  */
 int process_exec(struct io_intf * exeio){
     // First check arg
@@ -85,70 +90,19 @@ int process_exec(struct io_intf * exeio){
     }
     // Then unmap virtual memory mapping of other user process
     memory_unmap_and_free_user();
-    // Initialize a new ptr for process 
-    struct process* cur_proc = NULL;
-    // Now we should first check and find avaible slot for user process
-    // Notice there are maximum of 16 concurrent process
-    for (int i = 0; i < NPROC; i++){
-        // If we find the empty slot
-        if (proctab[i] == NULL){
-            // TBD 2
-            cur_proc = kmalloc(sizeof(struct process));
-            // Return if fail to allocate space
-            if (!cur_proc){
-                // Memory allocate failure
-                return -EACCESS;
-            }
-            console_printf("New space for process created.\n");
-            // If successfully allocate space
-            // 1. Clear it
-            memset(cur_proc, 0, sizeof(struct process));
-            // 2. allocate process id 
-            cur_proc->id = i;
-                // struct thread *current_thread = CURTHR;
-                // int tid = current_thread->id;
-                // #define CURTHR ((struct thread*)__builtin_thread_pointer())
-
-            // 3. allocate thread id 
-            // Since we are having at most user process for this cp, we can set it to 0 for now
-            // TBD 3
-            cur_proc->tid = 0;
-
-                // uintptr_t new_page_table = memory_space_create(cur_proc->id);
-                // if (!new_page_table){
-                //     kfree(cur_proc);
-                //     proctab[i] = NULL;
-                //     return -ENOMEM;
-                // }
-                // cur_proc->mtag = new_page_table;
-            // Create root page table
-            cur_proc->mtag = active_memory_space();
-            //struct pte *root = mtag_to_root(new_page_table);
-            proctab[i] = cur_proc;
-            console_printf("Process initialized. proc_num: %d\n", i);
-            break;
-        }
-    }
-    // Check if reach max proc cnt
-    if (!cur_proc) {
-        return -EMFILE; 
-    }
+    
     // Initialize entry point
     void (*entry_point)(void) = NULL;
     // Run elf_load to update entry_point
     int elf_result = elf_load(exeio, &entry_point);
     // Check elf_result
-    if (elf_result < 0) {
-        memory_space_reclaim(); 
-        kfree(cur_proc);                      
-        proctab[cur_proc->id] = NULL;         
+    if (elf_result < 0) {       
         return elf_result;                    
     }
     console_printf("Elf successfully loaded. Entry point: %p\n", entry_point);
 
     // This is the staring pt of user stack
-    //uintptr_t usp = USER_STACK_VMA;
-    uintptr_t usp = USER_END_VMA ;
+    uintptr_t usp = USER_STACK_VMA;
     // Now change to user mode
     thread_jump_to_user(usp, entry_point);
     console_printf("Successfully to U mode\n");
@@ -171,10 +125,10 @@ void process_exit(){
     if (!cur_prog){
         panic("No current process exist");
     }
+
     // Release memory space
-    // TBD 4
-    memory_unmap_and_free_user();
-    memory_space_switch(cur_prog->mtag);
+    memory_space_reclaim();
+
     // Release I/O interfaces
     for (int i = 0; i < NPROC; i++){
         if (cur_prog->iotab[i]){
@@ -184,7 +138,8 @@ void process_exit(){
     }
     // mark it empty in the process table
     proctab[cur_prog->id] = NULL;
-    kfree(cur_prog);
+    //kfree(cur_prog);
+    
     // Exit the thread
     thread_exit();
 }
