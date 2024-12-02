45c190c91cdd5579137db1cabbaeadf846f65892
junyanb2 <junyanb2@fastx3-03.ews.illinois.edu>
2024-12-02 15:28:23 -0600

    Dec.2 minor amendment; buglog; memory interface; new bash

--- src/user/user_access_kern.c
+++ src/user/user_access_kern.c
@@ -5,5 +5,7 @@ typedef unsigned long uintptr_t;
 
 void main(void) {
     uintptr_t kern_addr = 0x80003000;
-    *((volatile uintptr_t *)kern_addr) = 1000;
+    //*((volatile uintptr_t *)kern_addr) = 1000;
+    size_t result = *((volatile uintptr_t *)kern_addr);
+    if (result) {}
 }
