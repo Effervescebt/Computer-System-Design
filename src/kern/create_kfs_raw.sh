c882e0892b17b87e292d0bc2a0c73d69d85e6691
Haoru Li <haorul2@illinois.edu>
2024-12-08 16:45:27 -0600

    Dec.8  user test; fixing test_lock

--- src/kern/create_kfs_raw.sh
+++ src/kern/create_kfs_raw.sh
@@ -8,7 +8,7 @@ make
 cd ../util
 make clean
 make
-./mkfs kfs.raw ../user/bin/init0 ../user/bin/init1 ../user/bin/init2 ../user/bin/trek ../user/bin/rule30 ../user/bin/init_trek_rule30 ../user/bin/init_fib_rule30 ../user/bin/init_fib_fib ../user/bin/fib ../user/bin/zork ../user/bin/rogue
+./mkfs kfs.raw ../user/bin/init0 ../user/bin/init1 ../user/bin/init2 ../user/bin/trek ../user/bin/rule30 ../user/bin/init_trek_rule30 ../user/bin/init_fib_rule30 ../user/bin/init_fib_fib ../user/bin/fib ../user/bin/zork ../user/bin/rogue ../user/bin/test_lock_file.txt ../user/bin/test_lock
 mv kfs.raw ../kern
 cd ../kern
 bash mkcomp.sh kfs.raw
